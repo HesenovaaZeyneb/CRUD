379df864d68488f657659ca0ee23b90bddd2b417
HesenovaaZeyneb <tu7wsdwla@code.edu.az>
2024-05-09 22:25:48 +0400

    commit

---
+++ Areas/Manage/Controllers/CategoryController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using mvc_1.DAL;
+using mvc_1.Models;
+
+namespace mvc_1.Areas.Manage.Controllers
+{   
+    [Area("Manage")]
+    public class CategoryController:Controller
+    {
+		
+		AppDbContext _context;
+        public CategoryController(AppDbContext dbcontext)
+        {
+            _context = dbcontext;
+            
+        }
+        public IActionResult Index() 
+        {
+            List<Category>categories=_context.Categories.Include(x=>x.Products).ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Update(int id)
+        {
+            var category=_context.Categories.FirstOrDefault(x=>x.Id == id);
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Update(Category category) 
+        {
+            var oldcategory = _context.Categories.FirstOrDefault(x => x.Id == category.Id);
+            if (oldcategory == null)
+            {
+                return RedirectToAction("Index");
+            }
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            oldcategory.Name = category.Name;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Areas/Manage/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc_1.DAL;
+using mvc_1.Models;
+
+namespace mvc_1.Areas.Manage.Controllers
+{
+	[Area("Manage")]
+	public class ProductsController:Controller
+	{
+		AppDbContext _db;
+        public ProductsController(AppDbContext dbcontext)
+        {
+            _db = dbcontext;
+        }
+        public IActionResult Index()
+		{
+			List<Product> products = _db.Products.Include(x=>x.Category).ToList();
+
+			
+			return View(products);
+		}
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Product product)
+		{
+			if(!ModelState.IsValid)
+			{
+				return View();
+			}
+			_db.Products.Add(product);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public IActionResult Update(int Id)
+		{
+			var product=_db.Products.FirstOrDefault(x => x.Id == Id);
+			if(product == null)
+			{
+				return RedirectToAction("Index");
+			}
+			return View(product);
+		}
+		[HttpPost]
+		public IActionResult Update(Product product)
+		{
+			var oldProduct = _db.Products.FirstOrDefault(y => y.Id == product.Id);
+			if(oldProduct == null)
+			{
+				return RedirectToAction("Index");
+			}
+			if(!ModelState.IsValid)
+			{
+				return View();
+			}
+			oldProduct.Name = product.Name;
+			oldProduct.Price= product.Price;
+			oldProduct.Description = product.Description;
+			oldProduct.Category = product.Category;
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+			
+
+		}
+		public IActionResult Delete(int id) 
+		{
+			var product = _db.Products.FirstOrDefault(x => x.Id == id);
+			if(product != null)
+			{
+				_db.Products.Remove(product);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		
+	}
+}
